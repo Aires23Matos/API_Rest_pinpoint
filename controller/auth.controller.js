a961e77ae7bbddb80d2349f37746a77cef658259
Aires23Matos <aires2matos18@gmail.com>
2025-06-05 12:42:15 +0100

    create folde utils with file generateTokenAndSetCookie ande generateVerificationCode

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.models.js";
+import { generateVerificationCode } from "../utils/generateVerificationCode.js";
 
 export const signUp = async (req, res) => {
     const {email, password,name } = req.body;
@@ -12,7 +13,22 @@ export const signUp = async (req, res) => {
             return res.status(400).json({success:false, message: 'User already exists'})
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const verificationCode = generateVerificationCode();
+
+        const user = new User({
+          email,
+          password: hashedPassword,
+          name,
+          verificationToken,
+          verificationTokenExpireAt: Date.now() + 24 * 60 *60 *1000
+        });
+
+        await user.save();
+
+        //jwt
+        generateTokenAndSetCookie(res, user._id);
+
     }catch(error){
         res.status(400).json({success: false, message: error.message})
     }
